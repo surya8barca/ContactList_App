e9ac252a7840dc837665007d92e641afd23017bf
surya8barca <surya8barca@gmail.com>
2020-04-18 20:23:56 +0530

    update and delete function added

--- contacts/lib/editData.dart
+++ contacts/lib/editData.dart
@@ -1,77 +1,89 @@
 
+
 import 'package:contacts/database.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() => runApp(MaterialApp(
-  home: Add(),
+  home: Edit(),
 )
 );
 
-class Add extends StatefulWidget {
+class Edit extends StatefulWidget {
+  final int index;
+  Edit({this.index});
+
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Add> {
-  final database= Hive.box('database');
+class _HomeState extends State<Edit> {
+
+  final database = Hive.box('database');
   TextEditingController forname=new TextEditingController();
   TextEditingController forage=new TextEditingController();
   TextEditingController forphone=new TextEditingController();
   TextEditingController foremail=new TextEditingController();
+
   String name;
   int age;
   String phone;
   String email;
 
-  checkinvalid(){
-    Pattern emailType= r'^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$';
-    RegExp regex= new RegExp(emailType);
-    if(name==null||age==null||phone==null||email==null)
-    {
-      Alert(context: context,title: 'All fields are required',buttons: [],style: AlertStyle(backgroundColor:Colors.cyan)).show();
-      return true;
-    }
-    else if(name=='')
+  validName(str){
+    if(str=='')
     {
       Alert(context: context,title: 'Invalid Name',desc: "Please Enter a valid name",buttons: [],style: AlertStyle(backgroundColor:Colors.cyan)).show();
-      return true;
+      return false;
     }
-    else if(age<1||age>100)
+    else  
+      return true;
+  }
+
+  validAge(str){
+    if(int.parse(str)<1||int.parse(str)>100)
     {
       Alert(context: context,title: 'Invalid Age',desc: "Please Enter a valid age\n(1 to 100)",buttons: [],style: AlertStyle(backgroundColor:Colors.cyan)).show();
-      return true;
+      return false;
     }
-    else if(phone.length!=10)
+    else  
+      return true;
+  }
+
+  validPhone(str){
+    if(str.length!=10)
     {
-      Alert(context: context,title: 'Invalid Phone Number',desc: "Please Enter a valid phone number\n(10 digits only)",buttons: [],style: AlertStyle(backgroundColor:Colors.cyan)).show();
-      return true;
+      Alert(context: context,title: 'Invalid Age',desc: "Please Enter a valid age\n(1 to 100)",buttons: [],style: AlertStyle(backgroundColor:Colors.cyan)).show();
+      return false;
     }
-    else if(!regex.hasMatch(email))
+    else  
+      return true;
+  }
+
+  validEmail(str){
+    Pattern emailType= r'^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$';
+    RegExp regex= new RegExp(emailType);
+    if(!regex.hasMatch(email))
     {
       Alert(context: context,title: 'Invalid Email Address',desc: "Please Enter a valid email address(example@site.domain)",buttons: [],style: AlertStyle(backgroundColor:Colors.cyan)).show();
-      return true;
-    }
-    else
       return false;
+    }
+    else  
+      return true;
   }
-
-  add(){
-    Database newdata = Database(name, age, phone, email);
-    database.add(newdata);
-  }
-
+  
 
   @override
   Widget build(BuildContext context) {
+    final data = database.getAt(widget.index) as Database;
     return Scaffold(
       backgroundColor: Colors.cyanAccent,
       appBar: AppBar(
         backgroundColor: Colors.blueGrey,
         centerTitle: true,
         title: Text(
-          'Add Data',
+          '${data.name}',
           style: TextStyle(
             fontSize: 28.0,
             color: Colors.black,
@@ -90,7 +102,7 @@ class _HomeState extends State<Add> {
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
                     Text(
-                      'Enter contact details',
+                      'Details of ${data.name}',
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         fontSize: 30,
@@ -103,13 +115,13 @@ class _HomeState extends State<Add> {
                       padding: const EdgeInsets.all(5.0),
                       child: TextField(
                         controller: forname,
-                        textInputAction: TextInputAction.next,
+                        textInputAction: TextInputAction.done,
                         decoration: InputDecoration(
                           alignLabelWithHint: true,
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(50),
                           ),
-                          labelText: 'Full Name:',
+                          labelText: 'Name: ${data.name}',
                           labelStyle: TextStyle(
                             fontSize: 25,
                             color: Colors.black,
@@ -118,9 +130,10 @@ class _HomeState extends State<Add> {
                         ),
                         onSubmitted: (value){
                           setState(() {
-                            name=value;  
+                            if(validName(value))
+                                data.name=value;  
                           });
-                          FocusScope.of(context).nextFocus();
+                          FocusScope.of(context).unfocus();
                         },
                       ),
                     ),
@@ -130,24 +143,25 @@ class _HomeState extends State<Add> {
                       child: TextField(
                         controller: forage,
                         keyboardType: TextInputType.number,
-                        textInputAction: TextInputAction.next,
+                        textInputAction: TextInputAction.done,
                         decoration: InputDecoration(
                           alignLabelWithHint: true,
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(50),
                           ),
-                          labelText: 'Age:',
+                          labelText: 'Age: ${data.age}',
                           labelStyle: TextStyle(
                             fontSize: 25,
                             color: Colors.black,
                           ),
                           helperText: '*reqiured',
                         ),
-                        onSubmitted: (value){
+                          onSubmitted: (value){
                           setState(() {
-                            age=int.parse(value);  
+                            if(validAge(value))
+                                data.age=int.parse(value);  
                           });
-                          FocusScope.of(context).nextFocus();
+                          FocusScope.of(context).unfocus();
                         },
                       ),
                     ),
@@ -157,24 +171,25 @@ class _HomeState extends State<Add> {
                       child: TextField(
                         controller: forphone,
                         keyboardType: TextInputType.number,
-                        textInputAction: TextInputAction.next,
+                        textInputAction: TextInputAction.done,
                         decoration: InputDecoration(
                           alignLabelWithHint: true,
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(50),
                           ),
-                          labelText: 'Mobile Number:',
+                          labelText: 'Number: ${data.phone}',
                           labelStyle: TextStyle(
                             fontSize: 25,
                             color: Colors.black,
                           ),
                           helperText: '*reqiured',
                         ),
-                        onSubmitted: (value){
+                          onSubmitted: (value){
                           setState(() {
-                            phone=value;  
+                            if(validPhone(value))
+                                data.phone=value;  
                           });
-                          FocusScope.of(context).nextFocus();
+                          FocusScope.of(context).unfocus();
                         },
                       ),
                     ),
@@ -189,17 +204,19 @@ class _HomeState extends State<Add> {
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(50),
                           ),
-                          labelText: 'Email:',
+                          labelText: 'Email: ${data.email}',
                           labelStyle: TextStyle(
                             fontSize: 25,
                             color: Colors.black,
                           ),
                           helperText: '*reqiured',
                         ),
-                        onSubmitted: (value){
+                          onSubmitted: (value){
                           setState(() {
-                            email=value;  
+                            if(validEmail(value))
+                                data.email=value;  
                           });
+                          FocusScope.of(context).unfocus();
                         },
                       ),
                     ),
@@ -213,67 +230,10 @@ class _HomeState extends State<Add> {
                     buttonColor: Colors.lightBlue,
                     child: RaisedButton(
                     onPressed: (){
-                      if(!checkinvalid())
-                      {
-                        Alert(
-                        context: context,
-                        style: AlertStyle(
-                          backgroundColor: Colors.cyan,
-                        ),
-                        title: "Save",
-                        desc: "Are you sure you want to Save in database?",
-                        buttons: [],
-                        content: Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              ButtonTheme(
-                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                                buttonColor: Colors.black,
-                                child: RaisedButton(
-                                  onPressed: (){
-                                    Navigator.pop(context);
-                                  },
-                                  child: Text(
-                                    'No',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              ButtonTheme(
-                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                                buttonColor: Colors.black,
-                                child: RaisedButton(
-                                  onPressed: (){
-                                    add();
-                                    Navigator.pop(context);
-                                    Navigator.pop(context);
-                                  },
-                                  child: Text(
-                                    'Yes',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ).show();
-                      }
+                      Navigator.pop(context);
                     }, 
                     child: Text(
-                      'Save',
+                      'Save Changes',
                       style: TextStyle(
                         color: Colors.black,
                         fontSize: 30.0,
@@ -284,7 +244,7 @@ class _HomeState extends State<Add> {
                 ],
                 ),
               ),
-      )
+      ),
     );
   }
 }
--- contacts/lib/main.dart
+++ contacts/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:contacts/addData.dart';
 import 'package:contacts/database.dart';
+import 'package:contacts/editData.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart' as pathProvider;
@@ -47,11 +48,118 @@ class _HomeState extends State<Home> {
           return ListView.builder(
             itemCount: database.length,
             itemBuilder: (context,index){
-              final data=database.getAt(index) as Database; //returns our data as our class instan
+              final data=database.getAt(index) as Database; //returns the data as our class instance
 
-              return Container(
-                //display the features with your funcions n shit
-
+              return Column(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blue),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          data.name,
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          data.phone,
+                        ),
+                        SizedBox(height: 15),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              FlatButton.icon(
+                                onPressed: (){},
+                                label: Text(
+                                  'View',
+                                  style: TextStyle(color: Colors.red)
+                                ),
+                                icon: Icon(Icons.remove_red_eye),
+                              ),
+                              FlatButton.icon(
+                                onPressed: (){
+                                  Route route = MaterialPageRoute(builder: (context)=> Edit(index:index));
+                                  Navigator.push(context, route);
+                                },
+                                label: Text(
+                                  'Edit ',
+                                  style: TextStyle(color: Colors.red)
+                                ),
+                                icon: Icon(Icons.edit),
+                              ),
+                              FlatButton.icon(
+                                onPressed: (){
+                                  Alert(
+                        context: context,
+                        style: AlertStyle(
+                          backgroundColor: Colors.cyan,
+                        ),
+                        title: "Delete",
+                        desc: "Are you sure you want to delete ${data.name}?",
+                        buttons: [],
+                        content: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              ButtonTheme(
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                                buttonColor: Colors.black,
+                                child: RaisedButton(
+                                  onPressed: (){
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text(
+                                    'No',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              ButtonTheme(
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                                buttonColor: Colors.black,
+                                child: RaisedButton(
+                                  onPressed: (){
+                                    database.deleteAt(index);
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text(
+                                    'Yes',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ).show();
+                                },
+                                label: Text(
+                                  'Delete',
+                                  style: TextStyle(color: Colors.red)
+                                ),
+                                icon: Icon(Icons.delete),
+                              ),
+                          ],
+                        ),
+                      ]
+                  ),
+                  ),
+                  SizedBox(height: 3),
+                ],
               );
             }
           );
