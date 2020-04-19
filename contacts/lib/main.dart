d225c49156e4ac69a4afa94c24964e5a2dab73f5
surya8barca <surya8barca@gmail.com>
2020-04-19 12:34:32 +0530

    call,message and email service added

--- contacts/lib/main.dart
+++ contacts/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart' as pathProvider;
 import 'package:hive/hive.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 void main()async{
@@ -54,8 +55,58 @@ class _HomeState extends State<Home> {
                 onTap: (){
                   Alert(
                     context: context,
-                    title: 'Options',
-                    buttons: [],                    
+                    title: '${data.name}',
+                    buttons: [],
+                    style: AlertStyle(
+                      backgroundColor: Colors.transparent,
+                      titleStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                      )
+                    ),
+                    content: Column(
+                      children: <Widget>[
+                        SizedBox(height: 25),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            IconButton(
+                              onPressed: (){
+                                launch("tel: ${data.phone}");
+                              },
+                              icon: Icon(
+                                Icons.call,
+                                size: 60,
+                                color: Colors.green,
+                              ),
+                            ),
+                            SizedBox(width: 35),
+                            IconButton(
+                              onPressed: (){
+                                launch("sms: ${data.phone}");
+                              },
+                              icon: Icon(
+                                Icons.message,
+                                size: 60,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(width: 35),
+                            IconButton(
+                              onPressed: (){
+                                launch("mailto: ${data.email}");
+                              },
+                              icon: Icon(
+                                Icons.email,
+                                size: 60,
+                                color: Colors.red,
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                          ],
+                        ),
+                      ],
+                    )
                   ).show();
                 },
                   child: Column(
@@ -106,7 +157,9 @@ class _HomeState extends State<Home> {
                                   Alert(
                         context: context,
                         style: AlertStyle(
-                          backgroundColor: Colors.cyan,
+                          backgroundColor: Colors.transparent,
+                          titleStyle: TextStyle(color:Colors.white),
+                          descStyle:  TextStyle(color:Colors.white),
                         ),
                         title: "Delete",
                         desc: "Are you sure you want to delete ${data.name}?",
@@ -118,7 +171,7 @@ class _HomeState extends State<Home> {
                             children: <Widget>[
                               ButtonTheme(
                                 shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                                buttonColor: Colors.black,
+                                buttonColor: Colors.white,
                                 child: RaisedButton(
                                   onPressed: (){
                                     Navigator.pop(context);
@@ -129,14 +182,14 @@ class _HomeState extends State<Home> {
                                     style: TextStyle(
                                       fontSize: 20,
                                       fontWeight: FontWeight.bold,
-                                      color: Colors.white,
+                                      color: Colors.black,
                                     ),
                                   ),
                                 ),
                               ),
                               ButtonTheme(
                                 shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                                buttonColor: Colors.black,
+                                buttonColor: Colors.white,
                                 child: RaisedButton(
                                   onPressed: (){
                                     database.deleteAt(index);
@@ -148,7 +201,7 @@ class _HomeState extends State<Home> {
                                     style: TextStyle(
                                       fontSize: 20,
                                       fontWeight: FontWeight.bold,
-                                      color: Colors.white,
+                                      color: Colors.black,
                                     ),
                                   ),
                                 ),
